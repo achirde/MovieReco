37cff180d482bde2f3aee22cfcdf44ead2629704
Arkadipra De <Arkadipra@imac.local>
2017-06-26 12:36:02 -0700

    Initial Commit

---
+++ MovieReco/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieReco
+//
+//  Created by Arkadipra De on 6/7/17.
+//  Copyright © 2017 Achirangshu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
